4e42eecac851ebbe16f1cf2de5792a2b0672d151
Kattrigerkatta <66407275+Kattrigerkatta@users.noreply.github.com>
2022-04-11 03:47:03 +0200

    First Upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'KattrigerKatta'
+
+files {
+    'html/index.html'
+}
+
+ui_page 'html/index.html'
+
+client_scripts {
+    'locales.lua',
+    'config.lua',
+    'client.lua',
+}
